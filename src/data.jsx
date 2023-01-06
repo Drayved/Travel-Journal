fbf0f764944896a94f0f10aeeb3cf439d5fd68e3
David Riley <Dril112191@gmail.com>
2023-01-05 22:48:02 -0500

    made responsive

--- src/data.jsx
+++ src/data.jsx
@@ -8,15 +8,7 @@ export default [
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
-    {
-        title: "Sydney Opera House",
-        location: "Australia",
-        googleMapsUrl: "https://www.google.com/maps/d/u/0/viewer?mid=1NYBTZlF3StQEf65_sVbpMlskCoo&hl=en&ll=-33.856910000000006%2C151.215262&z=17",
-        startDate: "27 May, 2021",
-        endDate: "8 Jun, 2021",
-        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
-    },
+
     {
         title: "Geirangerfjord",
         location: "Norway",
@@ -25,5 +17,16 @@ export default [
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    },
+
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://www.google.com/maps/d/u/0/viewer?mid=1NYBTZlF3StQEf65_sVbpMlskCoo&hl=en&ll=-33.856910000000006%2C151.215262&z=17",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
     }
+
 ]
